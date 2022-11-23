4ad743e41842c30c33d725ba3293e85818fe296e
Lucas de Carvalho Barbosa <103704337+LucasCBar@users.noreply.github.com>
2022-11-23 00:36:26 -0300

    Add files via upload

--- src/backend/src/routes/provas.routes.ts
+++ src/backend/src/routes/provas.routes.ts
@@ -35,5 +35,9 @@ const upload: Multer = multer({
 
 router.post('/',upload.single("file"), provasController.cadastrarProva);
 router.post('/Prova', provasController.retornaProva);
+router.post('/EntregarProva',upload.single("file"), provasController.cadastrarProvaFeita);
+router.post('/ProvasAprendiz', provasController.provasFeitas);
+router.get('/ProvasCorrigir', provasController.provasSemNota);
+router.post('/ProvasNota', provasController.cadastrarNota);
 
 export default router;
